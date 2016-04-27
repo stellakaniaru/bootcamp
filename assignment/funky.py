d9a5e96c1f9a8eccd4c979470d8bae47e452b305
stellakaniaru16@gmail.com <stellakaniaru16@gmail.com>
2016-04-27 04:48:33 +0300

    funky_function

---
+++ assignment/funky.py
@@ -0,0 +1,14 @@
+def funky(a , b):
+	try:
+		if type(a) and type(b) == int or float:
+			return(a + b)
+		if type(a) and type(b) == list:
+			return(a + b)
+		if type(a) and type(b) == str:
+			return(a + b)
+	except TypeError as e:
+		e = funky
+		return 'operand not supported'
+
+stl = funky('ste','ret')
+print stl
