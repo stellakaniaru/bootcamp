82bfed6b206c0683e8cc9dfce4276c1a7278c62e
stellakaniaru16@gmail.com <stellakaniaru16@gmail.com>
2016-04-28 16:29:26 +0300

    refract test_super_sum

--- day_4/super_sum.py
+++ day_4/super_sum.py
@@ -1,10 +1,27 @@
-def super_sum():
+def super_sum(*args):
 	'''
 	returns a sum of numbers.
 	e.g
-	   super_sum() ==> "please enter numbers"
+	   super_sum() ==> "0
+	   super_sum('string') ==>
 	   super_sum(1, 2, 3) ==> 6
 	   super_sum([1, 2, 3]) ==> 6
 	   super_sum([10],[20, 30]) ==>60
 	   '''
-	pass
+	total = 0
+	if not args:
+		return 0
+
+	else:
+		for x in args:
+			if type(x) == list:
+				total += sum(x)
+			elif type(x) == str:
+				return 0
+			else:
+				total += x
+
+		return total
+
+
+
--- day_4/test_super_sum.py
+++ day_4/test_super_sum.py
@@ -11,5 +11,25 @@ class SuperSumTestCase(TestCase):
     def test_empty_input(self):
     	'''test empty input.'''
 
-    	self.assertEqual(super_sum(), "please enter numbers")
+    	self.assertEqual(super_sum(), 0)
+
+
+    def test_sum_of_integers(self):
+    	'''test sum of integers'''
+
+    	self.assertEqual(super_sum(1, 2, 3), 6)
+    	self.assertEqual(super_sum(-1, 1), 0)
+    	self.assertNotEqual(super_sum(10, 20, 30), 100)
+
+    def test_string_input_returns(self):
+    	'''test string inputs returns'''
+
+    	self.assertEqual(super_sum('string', 1, 4), 0)
+
+    def test_sum_of_items_in_one_list(self):
+    	'''test sum of items in a single string'''
+
+     	self.assertEqual(super_sum([1, 2, 3]), 6)
+    	
+
 
